efe6a11dca6bfe4d2e893c8d4913f27372711c2e
azzilak <azzilakaren@students.amikom.ac.id>
2021-04-13 12:12:12 +0700

    Add project files.

---
+++ ResponsiPemrograman3312/Karyawan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3312
+{
+    public class Karyawan
+    {
+            public Karyawan(int Urut, int nik, string Name, int Gaji)
+            {
+                Urutan = Urut;
+                NIK = nik;
+                Nama = Name;
+                GajiBulanan = Gaji;
+
+            }
+
+            public int Urutan { get; set; }
+            public int NIK { get; set; }
+            public string Nama { get; set; }
+            public int GajiBulanan { get; set; }
+
+        public void Output1()
+        {
+            Console.WriteLine("{0}. {1}  {2}           {3}", Urutan, NIK, Nama, GajiBulanan);
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("Maaf Gaji kurang dari ");
+            }
+
+        }
+        public void Output2()
+        {
+            Console.WriteLine("{0}. {1}  {2}           {3}", Urutan, NIK, Nama, GajiBulanan+ (GajiBulanan * 0.1));
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("Maaf Gaji kurang dari ");
+            }
+        }
+
+        //public void Output2()
+        //{
+        // Console.WriteLine("{0}. {1}   {2}         {3}", Urutan, NIK, Nama, GajiBulanan + (GajiBulanan * 0.1));
+
+        //}
+    }
+}
---
+++ ResponsiPemrograman3312/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3312
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No. Nik/Nama                   Gaji Bulanan");
+            Console.WriteLine("-------------------------------------------");
+                
+            Karyawan karyawan1 = new Karyawan(1, 190302123, "Azzila ", 3000000);
+            karyawan1.Output1();
+
+            Karyawan karyawan2 = new Karyawan(2, 190302124, "Karen   ", 2000000);
+            karyawan2.Output1();
+
+            Console.WriteLine("");
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%!!!");
+            Console.WriteLine("");
+
+            Console.WriteLine("");
+            Console.WriteLine("No Nik/Nama                   Gaji Bulanan");
+            Console.WriteLine("------------------------------------------");
+            Karyawan kenaikan1 = new Karyawan(1, 190302123, "Azzila ", 3000000);
+            kenaikan1.Output2();
+            Karyawan kenaikan2 = new Karyawan(2, 190302124, "Karen   ", 2000000);
+            kenaikan2.Output2();
+            Console.ReadKey(true);
+        }
+    }
+
+}
